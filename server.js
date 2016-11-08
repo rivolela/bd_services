809d57796c9a3c98c12439166e3fa9b0a23c8c2b
rivolela <contato@pickoout.com.br>
2016-11-08 13:54:24 +0000

    add method get list offers crawlers

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
@@ -7,7 +7,19 @@ var mongoose = require('./config/mongoose'),
 var db = mongoose();
 var app = express();
 var passport = passport();
-app.listen(3000); 
+//app.listen(3000); 
+
+var server_port;
+
+if(process.env.NODE_ENV == 'production'){
+	server_port = process.env.PORT || 80;
+}else{
+	server_port = 3000;
+}
+
+app.listen(server_port,function() {
+    console.log('Server runnning on port %d', server_port);
+});
 
 module.exports = app; 
 
