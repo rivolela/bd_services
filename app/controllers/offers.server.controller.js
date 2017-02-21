fb9acf8253013b0b3818036f7c98d8a7d0bcd5af
rivolela <contato@pickoout.com.br>
2017-02-21 09:27:05 +0000

    change sort filter

--- app/controllers/offers.server.controller.js
+++ app/controllers/offers.server.controller.js
@@ -128,17 +128,17 @@ var filter_offers = function(filter,next){
 	// filter by total of reviews and after higher score
     case 1:
         resp = {
-        	totalReviews: -1
+        	totalReviews: -1,
         	score: { 
 				$meta: "textScore" 
 			},
-			score: -1,
+			score: -1
 		};
         break;
     // filter by total of countHappy and after higher score
     case 2:
         resp = {
-        	countHappy: -1
+        	countHappy: -1,
         	score: { 
 				$meta: "textScore" 
 			},
@@ -148,11 +148,11 @@ var filter_offers = function(filter,next){
     // filter by total of countSad and after higher score
     case 3:
         resp = {
-        	countSad: -1
+        	countSad: -1,
         	score: { 
 				$meta: "textScore" 
 			},
-			score:-1,
+			score:-1
 		};
         break;
     
